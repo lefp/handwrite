2b32667d3289da4c79081cb13571410eb2f7604e
Peter Lef <lefp@clarkson.edu>
2023-03-08 19:16:33 -0500

    Bounding boxes
    Signed-off-by: Peter Lef <lefp@clarkson.edu>

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ Canvas
     responsible for creating and keeping track of drawn strokes
     maybe responsible for displaying them? Or maybe that should should be done by some other component
 */
+// @todo can replace some uses of #[cfg(debug_assertions)] with more fine-grained switches, like one
+// specifically for showing bounding boxes
 
 use macroquad::prelude::*;
 
@@ -17,22 +19,73 @@ impl Point {
     fn new(x: f32, y: f32) -> Self {
         Point { x, y }
     }
+
+    /// Component-wise min.
+    fn min(&self, p: Point) -> Point {
+        Point { x: self.x.min(p.x), y: self.y.min(p.y) }
+    }
+    /// Component-wise max.
+    fn max(&self, p: Point) -> Point {
+        Point { x: self.x.max(p.x), y: self.y.max(p.y) }
+    }
+}
+impl From<(f32, f32)> for Point {
+    fn from(point: (f32, f32)) -> Self {
+        Self::new(point.0, point.1)
+    }
+}
+impl Into<(f32, f32)> for Point {
+    fn into(self) -> (f32, f32) {
+        (self.x, self.y)
+    }
 }
 
-// // a sequence of points. No thickness information attached!
-// struct Curve {
-//     points: Vec<Point>
-// }
-// impl Curve {
-//     fn new(first_point: Point) -> Self {
-//         Self { points: vec![first_point] }
-//     }
-//     fn add_point(&mut self, p: Point) {
-//         self.points.push(p);
-//     }
-// }
+/// A rectangle with no rotation information.
+/// Satisfies p1.x <= p2.x, p1.y <= p2.y
+struct Box {
+    p1: Point,
+    p2: Point,
+}
+impl Box {
+    /// Expands exactly as much as needed to contain `p`.
+    /// The new region always contains the old one; interior points are never lost.
+    fn expand_to_contain(&mut self, p: Point) {
+        self.p1 = self.p1.min(p);
+        self.p2 = self.p2.max(p);
+    }
+}
 
-type Curve = Vec<Point>;
+/// A sequence of points. No thickness information attached!
+/// The bounding box is provided to easily find curves intersecting a region; you can first check whether the
+/// bounding box intersect the region before checking every point on the curve, which allows you to quickly
+/// discard curves that are not near the region.
+/* @todo would it be more efficient to compute a bounding rectangle of minimal area? E.g. a diagonal line
+segment is much more precisely constrained by a rotated rectangle than a horizontal rectangle. This would
+reduce the number of curves whose bounding box intersect the index region. Some questions:
+- Is this efficient to compute?
+- Is this efficient to compute iteratively? (i.e. every time a new point is added to the curve)
+    Although, why would we ever need to compute it iteratively? New points are only added while the curve is
+    being drawn.
+- Is it easy to compute the intersection of a regular box with a rotated rectangle?
+*/
+struct Curve {
+    points: Vec<Point>,
+    bounding_box: Box,
+}
+impl Curve {
+    fn new(first_point: Point) -> Self {
+        Self {
+            points: vec![first_point],
+            bounding_box: Box { p1: first_point, p2: first_point },
+        }
+    }
+    fn add_point(&mut self, p: Point) {
+        self.points.push(p);
+        // update the bounding box
+        self.bounding_box.expand_to_contain(p);
+    }
+    // @todo fn erase()
+}
 
 #[derive(Default)]
 struct Canvas {
@@ -47,12 +100,13 @@ impl Canvas {
             self.current_curve.is_none(),
             "Attempted to start a new curve before ending the previous one"
         );
-        self.current_curve = Some(vec![first_point]);
+        let curve = Curve::new(first_point);
+        self.current_curve = Some(curve);
     }
     /// Add a point to the curve currently being drawn.
     pub fn continue_curve(&mut self, new_point: Point) {
         self.current_curve.as_mut().expect("Attempted to continue a curve that doesn't exist")
-            .push(new_point);
+            .add_point(new_point);
     }
     /// Stop drawing the current curve.
     /// It is illegal to subsequently call `continue_curve` or `end_curve` before `start_curve`.
@@ -64,11 +118,18 @@ impl Canvas {
 
     /// Render a single curve.
     fn render_curve(curve: &Curve) {
-        for endpoints in curve.windows(2) {
+        for endpoints in curve.points.windows(2) {
             let p1 = endpoints[0];
             let p2 = endpoints[1];
             draw_line(p1.x, p1.y, p2.x, p2.y, 3f32, BLUE);
         }
+
+        // draw the bounding box
+        #[cfg(debug_assertions)]
+        let (x1, y1) = curve.bounding_box.p1.into();
+        let (x2, y2) = curve.bounding_box.p2.into();
+        //
+        draw_rectangle_lines(x1, y1, x2 - x1, y2 - y1, 2f32, RED);
     }
     
     /// Render all objects on the canvas to the screen.
